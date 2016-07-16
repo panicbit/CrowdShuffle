4d44cac86d9e4e85eb6ce1dee449e72272b3c188
panicbit <panicbit.dev@gmail.com>
2016-07-16 15:22:12 +0200

    Add OpenCV dependency

--- CrowdShuffleVoter/app/src/main/java/com/github/panicbit/crowdshufflevoter/MainActivity.java
+++ CrowdShuffleVoter/app/src/main/java/com/github/panicbit/crowdshufflevoter/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // App init (maybe move to own class
+    static{
+        System.loadLibrary("opencv_java3");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
